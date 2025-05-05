f5838a388b1f11ec0f3c2f8b52a654c62a2208b9
Michael Little <michaellittle92@gmail.com>
2025-05-05 11:43:20 +1000

    getting weather overview from api

--- weather.py
+++ weather.py
@@ -2,15 +2,16 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.environ.get('WEATHER_API')
-try:
+def get_weather(): 
+    load_dotenv()
+    api_key = os.environ.get('WEATHER_API')
     url = 'https://api.openweathermap.org/data/2.5/weather?lat=-37.7194&lon=144.8916&appid=' + api_key
-
     resp = requests.get(url)
     data = resp.json()
+    weather = data["weather"]
+    main = weather[0]['main']
+    description =  weather[0]['description']
+    icon = weather[0]['icon']
+    return(main, description, icon)
 
-    print(data)
-except Exception as e:
-    print(e) 
